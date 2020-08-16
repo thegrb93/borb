a33bec804c79974c8f91a9f1e5a074367021e622
Garrett Brown <grbrown93@sbcglobal.net>
2020-08-16 04:20:15 -0400

    Fix color and add contacts

--- lib/windfield/init.lua
+++ lib/windfield/init.lua
@@ -78,9 +78,9 @@ function World:draw(alpha)
     -- get the current color values to reapply
     local r, g, b, a = love.graphics.getColor()
     -- alpha value is optional
-    alpha = alpha or 255
+    alpha = alpha or 1
     -- Colliders debug
-    love.graphics.setColor(222, 222, 222, alpha)
+    love.graphics.setColor(0.87, 0.87, 0.87, alpha)
     local bodies = self.box2d_world:getBodies()
     for _, body in ipairs(bodies) do
         local fixtures = body:getFixtures()
@@ -100,20 +100,33 @@ function World:draw(alpha)
             end
         end
     end
-    love.graphics.setColor(255, 255, 255, alpha)
+    love.graphics.setColor(1, 1, 1, alpha)
 
     -- Joint debug
-    love.graphics.setColor(222, 128, 64, alpha)
+    love.graphics.setColor(0.87, 0.5, 0.25, alpha)
     local joints = self.box2d_world:getJoints()
     for _, joint in ipairs(joints) do
         local x1, y1, x2, y2 = joint:getAnchors()
         if x1 and y1 then love.graphics.circle('line', x1, y1, 4) end
         if x2 and y2 then love.graphics.circle('line', x2, y2, 4) end
     end
-    love.graphics.setColor(255, 255, 255, alpha)
+    love.graphics.setColor(1, 1, 1, alpha)
+
+    -- Contact debug
+    love.graphics.setColor(0.87, 0.25, 0.25, alpha)
+    for _, contact in ipairs(self.box2d_world:getContacts()) do
+        local x1,y1,x2,y2 = contact:getPositions()
+        if x1 then
+            love.graphics.rectangle('fill', x1-0.1, y1-0.1, 0.2, 0.2)
+        end
+        if x2 then
+            love.graphics.rectangle('fill', x2-0.1, y2-0.1, 0.2, 0.2)
+        end
+    end
+    love.graphics.setColor(1, 1, 1, alpha)
 
     -- Query debug
-    love.graphics.setColor(64, 64, 222, alpha)
+    love.graphics.setColor(0.25, 0.25, 0.87, alpha)
     for _, query_draw in ipairs(self.query_debug_draw) do
         query_draw.frames = query_draw.frames - 1
         if query_draw.type == 'circle' then
